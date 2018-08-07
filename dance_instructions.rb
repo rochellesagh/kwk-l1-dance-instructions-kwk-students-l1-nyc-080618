226951dd88e6202e75f9d52a78fc353134e9b4e2
rochellesagh <rsrochelli@gmail.com>
2018-08-07 13:14:29 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,18 @@
+def Gangam_Style
+puts "Plant legs far apart, bend knees slightly and keep posture loose"
+puts "Lift right foot"
+puts "Return right foot to the ground"
+puts "Finishing with a small skip-step backward"
+puts "Lower left foot to the ground"
+puts "Bounce left foot back up slightly, kicking it a few inches back"
+puts "Left, Right, Left and Left"
+puts "Lower right foot to the ground"
+puts "Bounce right foot back up slightly, kicking it a few inches back"
+puts "Right, Left, Right and Right"
+puts "Holding your arms out in front of you, straight and at chest level"
+puts "Cross your right wrist over your left and hold them together"
+puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+end 
 
-# Code your methods below
+Gangam_Style
